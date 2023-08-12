45d63e96910f7ed4e2f56b05383705f4d20deced
İbrhaim Halid Bayrak <ibrahimh.bayrak@gmail.com>
2023-08-12 22:57:43 +0300

    veriler table'a işlnedi

---
+++ HaberProjesi/ViewModel/NewsViewModel.swift
@@ -0,0 +1,29 @@
+//
+//  NewsViewModel.swift
+//  HaberProjesi
+//
+//  Created by İbrahim Halid Bayrak on 12.08.2023.
+//
+
+import Foundation
+
+struct NewsTableViewModel {
+    let newsList : [News]
+    func numberOfRowsInSection () -> Int {
+        return self.newsList.count
+    }
+    func newsAtIndexPath(_ index: Int) -> NewsViewModel {
+        let news = self.newsList[index]
+        return NewsViewModel(news: news)
+    }
+}
+
+struct NewsViewModel {
+    let news : News
+    var title : String {
+        return self.news.title
+    }
+    var story : String {
+        return self.news.story
+    }
+}
