065fede9c7020d4e60cc026fa42af8629c5e3278
İbrhaim Halid Bayrak <ibrahimh.bayrak@gmail.com>
2023-08-12 22:02:47 +0300

    Initial Commit

---
+++ HaberProjesi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HaberProjesi
+//
+//  Created by İbrahim Halid Bayrak on 12.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
