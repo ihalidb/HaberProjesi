5dc14b7fc9c29a6edc4308b63ca8deb93aedeeb3
İbrhaim Halid Bayrak <ibrahimh.bayrak@gmail.com>
2023-08-12 22:28:36 +0300

    MVVM

---
+++ HaberProjesi/Model/News.swift
@@ -0,0 +1,17 @@
+//
+//  News.swift
+//  HaberProjesi
+//
+//  Created by İbrahim Halid Bayrak on 12.08.2023.
+//
+
+import Foundation
+
+struct News : Decodable {
+    let title: String
+    let story: String
+    
+    
+    // GET & POST
+    
+}
---
+++ HaberProjesi/Service/Webservice.swift
@@ -0,0 +1,15 @@
+//
+//  Webservice.swift
+//  HaberProjesi
+//
+//  Created by İbrahim Halid Bayrak on 12.08.2023.
+//
+
+import Foundation
+
+class Webservice {
+    
+    func haberleriIndir(url: URL, completion: ) {
+        
+    }
+}
