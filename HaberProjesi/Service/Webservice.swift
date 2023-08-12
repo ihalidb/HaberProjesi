f81f77767a03f99246dd1dc3cf909003acea66db
İbrhaim Halid Bayrak <ibrahimh.bayrak@gmail.com>
2023-08-12 22:44:05 +0300

    internetten veri çekildi.

--- HaberProjesi/Service/Webservice.swift
+++ HaberProjesi/Service/Webservice.swift
@@ -9,7 +9,19 @@ import Foundation
 
 class Webservice {
     
-    func haberleriIndir(url: URL, completion: ) {
+    func haberleriIndir(url: URL, completion: @escaping ([News]?) -> ()) {
         
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            if let error = error {
+                print(error.localizedDescription)
+                completion(nil)
+            } else if let data = data {
+                let haberlerDizisi = try? JSONDecoder().decode([News].self, from: data)
+                
+                if let haberlerDizisi = haberlerDizisi {
+                    completion(haberlerDizisi)
+                }
+            }
+        }.resume()
     }
 }
--- HaberProjesi/View/ViewController.swift
+++ HaberProjesi/View/ViewController.swift
@@ -15,6 +15,14 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         super.viewDidLoad()
         tableView.delegate = self
         tableView.dataSource = self
+        let url = URL(string: "https://raw.githubusercontent.com/atilsamancioglu/BTK-iOSDataSet/master/dataset.json")
+        
+        Webservice().haberleriIndir(url: url!) { (haberler) in
+            if let haberler = haberler {
+                print(haberler)
+            }
+        }
+        
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
